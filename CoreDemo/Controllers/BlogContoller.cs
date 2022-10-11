0e7549f9061e4276bcafe9971a01b9edaca6ec67
İbrahim Nureddin Işık <ibrahimnureddin@gmail.com>
2022-10-11 22:55:40 +0300

    Listing Blogs

--- CoreDemo/Controllers/BlogContoller.cs
+++ CoreDemo/Controllers/BlogContoller.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreDemo.Controllers
 {
     public class BlogContoller : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository()); 
         public IActionResult Index()
         {
-            return View();
+            var values =bm.GetList();
+            return View(values);
+
         }
     }
 }
